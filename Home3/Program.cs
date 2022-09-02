b1f970429fda463d745d165151d325087b3d3e47
Dmitry-Lab <poznavshii@gmail.com>
2022-09-02 15:50:23 +0400

    Home3

---
+++ Home3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+double [] array = new double [15];
+double max = array[0];
+double min = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-999, 1000)+ new Random().NextDouble();     // Добавляем дробную часть к числам
+
+    if( array[i] > max)
+    {
+        max = array[i];
+    }
+     if( array[i] < min)
+    {
+        min = array[i];
+    }
+}
+double answer = max - min;
+
+Console.WriteLine("[{0}]", String.Join(",", array));                         // Для проверки
+Console.WriteLine("Max: "+max+" Min: "+min);                                 // Для проверки
+Console.WriteLine("Разница = "+answer);
