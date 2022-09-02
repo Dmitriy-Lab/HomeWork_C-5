ae9cd173894ad5f2c99df4b2bdb47e79198c3a09
Dmitry-Lab <poznavshii@gmail.com>
2022-09-02 10:02:39 +0400

    Home1

---
+++ Home1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+int [] array = new int [10];
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+
+    if( array[i] % 2 == 0)
+    {
+        sum += 1;
+    }
+}
+
+Console.WriteLine("[{0}]", String.Join(",", array));  // Для проверки
+Console.WriteLine("Сумма четных чисел = "+sum);
